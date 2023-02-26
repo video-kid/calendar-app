3bed3543509d538d62b5e85be91ad726c23f275f
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2023-02-26 16:08:37 +0100

    add rest of tests

--- src/hooks/__tests__/utils.test.tsx
+++ src/hooks/__tests__/utils.test.tsx
@@ -1,7 +1,9 @@
 import {
   createCalendarDaysArray,
   createCalendarEmptyArray,
+  getEmptyCalendar,
   getPeriodRangeDate,
+  mergeEventsAndCalendar,
 } from "../utils";
 
 const event = {
@@ -94,4 +96,63 @@ test("creating empty calendar day objects array for given days (2 days here) ", 
   expect(createCalendarDaysArray(2, date)).toMatchObject(
     emptyCalendarDayObjectArrayOutput
   );
+});
+
+const eventsArray = [event];
+
+const filledCalendarOutput = [
+  {
+    day: new Date("2022-12-17T23:00:00.000Z"),
+    events: {
+      "666": {
+        day: new Date("2022-12-18T20:34:29.000Z"),
+        id: "666",
+        name: "event name 18-19 12 22",
+        startTime: "1671395669000",
+        endTime: "1671481532000",
+      },
+    },
+  },
+  {
+    day: new Date("2022-12-18T23:00:00.000Z"),
+    events: {
+      "666": {
+        day: new Date("2022-12-19T20:34:29.000Z"),
+        id: "666",
+        name: "event name 18-19 12 22",
+        startTime: "1671395669000",
+        endTime: "1671481532000",
+      },
+    },
+  },
+  {
+    day: new Date("2022-12-19T23:00:00.000Z"),
+    events: {},
+  },
+  {
+    day: new Date("2022-12-20T23:00:00.000Z"),
+    events: {},
+  },
+  {
+    day: new Date("2022-12-21T23:00:00.000Z"),
+    events: {},
+  },
+  {
+    day: new Date("2022-12-22T23:00:00.000Z"),
+    events: {},
+  },
+  {
+    day: new Date("2022-12-23T23:00:00.000Z"),
+    events: {},
+  },
+];
+
+test("Complete calendar array for week", () => {
+  expect(mergeEventsAndCalendar(eventsArray, "week", date)).toMatchObject(
+    filledCalendarOutput
+  );
+});
+
+test("Complete empty calendar array for week", () => {
+  expect(getEmptyCalendar("week", date)).toMatchObject(emptyWeekCalendarArray);
 });
